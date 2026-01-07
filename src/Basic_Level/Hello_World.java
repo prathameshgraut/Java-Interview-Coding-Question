3450c3722476cc856d9945eca49cdb88b7c514a2
Prathamesh Raut. <prathamesh1687@gmail.com>
2026-01-07 17:58:10 +0530

    Hello World Program

---
+++ src/Basic_Level/Hello_World.java
@@ -0,0 +1,11 @@
+package Basic_Level;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		System.out.println("👋Hello everyone,\nThis is first Java code and the start of Java coding journey.....");
+		System.out.println("Hello World");
+		System.out.println("Welcome To Enter ...😊 Java Programming! 😊");
+	}
+
+}
