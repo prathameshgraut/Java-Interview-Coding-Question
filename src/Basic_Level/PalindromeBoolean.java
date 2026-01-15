c0b04d3409f980a3bde3c8ef99e5cdddcfa362b5
Prathamesh Raut. <prathamesh1687@gmail.com>
2026-01-15 15:17:24 +0530

    Check Palindrome & Print Boolean Value

--- src/Basic_Level/PalindromeBoolean.java
+++ src/Basic_Level/PalindromeBoolean.java
@@ -1,5 +1,7 @@
 package Basic_Level;
 
+import java.util.Scanner;
+
 public class PalindromeBoolean {
 	
 	/*Using Hard Coded Data & Print Boolean Value = true OR false*/
@@ -18,7 +20,26 @@ public class PalindromeBoolean {
 			System.out.println(false);
 	}
 	
+	/*Use_input Method*/
+	static void user(int num) {
+		int sum=num,add,res=0;
+
+		while(num>0) {
+			add=num%10;
+			res=res*10+add;
+			num=num/10;
+		}
+		if(sum==res)
+			System.out.println(true);
+		else
+			System.out.println(false);
+	}
 public static void main(String[] args) {
 	print();
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter A Number =");
+	int num;
+	num=sc.nextInt();
+	user(num);
 }
 }
