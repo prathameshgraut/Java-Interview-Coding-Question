8683a4ff4e1fa8a6c27bc1f78d9e4477696c3346
Prathamesh Raut. <prathamesh1687@gmail.com>
2026-01-31 21:23:18 +0530

    Sum Of Diagonal's

--- src/Arrays_Program/Sum_Of_Digonals.java
+++ src/Arrays_Program/Sum_Of_Digonals.java
@@ -1,26 +1,32 @@
 package Arrays_Program;
 
+import java.util.Arrays;
+
 public class Sum_Of_Digonals {
-	public static void main(String[] args) {
-		int arr[][]={
+	static void diagonals() {
+		int arr[][]= {
 				{1,2,3},
 				{4,5,6},
 				{7,8,9}
 		};
 		
 		int len=arr.length;
-		int primaryDiagonals=0;
-		int secondaryDiagonal=0;
+		int primaryD=0,secondaryD=0;
 		
 		for(int i=0;i<len;i++) {
-			primaryDiagonals=primaryDiagonals+arr[i][i];
-			secondaryDiagonal=secondaryDiagonal+arr[i][len-i-1];
+			primaryD=primaryD+arr[i][i];
+			secondaryD=secondaryD+arr[i][len-i-1];
 		}
-		System.out.println("Primary Diagonal's :"+primaryDiagonals+"\nSecondary Diagonal's :"+secondaryDiagonal);
-		int sum=primaryDiagonals+secondaryDiagonal;
+		System.out.println("Primary Diagonals   :"+primaryD);
+		System.out.println("Secondary Diagonals :"+secondaryD);
+		int sum=primaryD+secondaryD;
+		
 		if(len%2==1) {
 			sum=sum-arr[len/2][len/2];
 		}
-		System.out.println("Sum Of Diagonal's :"+sum);
+		System.out.println("Sum Of Diagonal's   :"+sum);
+	}
+	public static void main(String[] args) {
+		diagonals();
 	}
 }
