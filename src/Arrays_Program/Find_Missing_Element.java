7bf5c9b6898c0e32e1d630174a803eeeff6cef23
Prathamesh Raut. <prathamesh1687@gmail.com>
2026-02-01 14:10:09 +0530

    Find Missing Element In Array

--- src/Arrays_Program/Find_Missing_Element.java
+++ src/Arrays_Program/Find_Missing_Element.java
@@ -3,37 +3,35 @@ package Arrays_Program;
 public class Find_Missing_Element {
 	
 	/*This Method Are Print Single Missing Element*/
-	static void Single_Missing() {
-		int arr[]= {1,2,3,5};
-		int Missing_Element=0;
-		
+	private static void Single_Missing(int arr[]) {
+		System.out.println("Show Single Missing Element,");
+		int missing=0;
 		for(int i=0;i<arr.length;i++) {
 			if(arr[i]!=i+1) {
-				Missing_Element=i+1;
+				missing = i+1;
+				break;
 			}
 		}
-		System.out.println("Show Missing Number :"+Missing_Element);
+		System.out.println("Show Missing Number :"+missing);
 	}
 	
-	
-	/*This Method Are Show All Missing Element*/
-	static void Multiple_Missing() {
-		int arr[]= {11,12,14,15};
-		int Missing_Element=1;
+	/*This Method Show All Missing Number*/
+	private static void Multiple_Missing(int arr[]) {
+		System.out.println("\n\nShow Multiple Missing Element,");
+		int missing=1;
 		int i=0;
-		
 		while(arr[i]<=arr[arr.length-1]) {
-			if(arr[i]==Missing_Element) {
+			if(arr[i]==missing) {
 				i++;
 			}else {
-				System.out.print(Missing_Element+" ");
+				System.out.print(missing+" ");
 			}
-			Missing_Element++;
+			missing++;
 		}
 	}
-	
 public static void main(String[] args) {
-	Single_Missing();
-	Multiple_Missing();
+	int arr[]= {1,2,3,5,8,10,13,15,18,20};
+	Single_Missing(arr);
+	Multiple_Missing(arr);
 }
 }
