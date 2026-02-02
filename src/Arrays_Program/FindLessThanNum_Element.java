01578bee992007600dd80eab5370f7ed9bfa4a5a
Prathamesh Raut. <prathamesh1687@gmail.com>
2026-02-02 12:46:31 +0530

    Find LessThan Number Element

---
+++ src/Arrays_Program/FindLessThanNum_Element.java
@@ -0,0 +1,25 @@
+package Arrays_Program;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FindLessThanNum_Element {
+	static void display() {
+		int arr[]= {10,20,30,40,50,60,70,80,90,100};
+		System.out.println("Show Array :"+Arrays.toString(arr));
+		int num;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter A Number :");
+		num=sc.nextInt();		
+		
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]<=num){
+				System.out.print(arr[i]+" ");
+		}
+		}
+	}
+	
+public static void main(String[] args) {
+	display();
+}
+}
