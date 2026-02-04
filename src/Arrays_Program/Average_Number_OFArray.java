b0f9e6d3af98f433d3a97100f643478d7abbb131
Prathamesh Raut. <prathamesh1687@gmail.com>
2026-02-04 18:47:05 +0530

    Find Average Numbers Of Array

---
+++ src/Arrays_Program/Average_Number_OFArray.java
@@ -0,0 +1,17 @@
+package Arrays_Program;
+
+public class Average_Number_OFArray {
+	
+	 void look() {
+		 System.out.println("Print Average Number Of Array....!");
+		 
+		 /*Using User Input To Stored In Array*/
+		 
+	 }
+	
+public static void main(String[] args) {
+	Average_Number_OFArray ANA = new Average_Number_OFArray();
+	
+	ANA.look();
+}
+}
