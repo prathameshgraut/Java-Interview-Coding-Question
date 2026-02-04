5922a2f6be6a2aa9b23ec593fa102dc60acd1126
Prathamesh Raut. <prathamesh1687@gmail.com>
2026-02-04 18:51:36 +0530

    Find Average Numbers Of Array

--- src/Arrays_Program/Average_Number_OFArray.java
+++ src/Arrays_Program/Average_Number_OFArray.java
@@ -1,12 +1,26 @@
 package Arrays_Program;
 
+import java.util.*;
+
 public class Average_Number_OFArray {
 	
 	 void look() {
 		 System.out.println("Print Average Number Of Array....!");
 		 
 		 /*Using User Input To Stored In Array*/
+		 int arr[]=new int[5];
+		 Scanner px = new Scanner(System.in);
+		 System.out.print("Enter A Value :");
+		 for(int i=0;i<arr.length;i++) {
+			 arr[i]=px.nextInt();
+		 }
+		 System.out.println("Show Array :"+Arrays.toString(arr));
 		 
+		 int avg=0;
+		 for(int j=0;j<arr.length;j++) {
+			 avg=avg+arr[j];
+		 }
+		 System.out.println("Average Numbers Of Array :"+avg);  //Counting All Element In array And Print Average Number In Preset At Array
 	 }
 	
 public static void main(String[] args) {
