7a1f95a4c141179279a4bb94da81a9c04d694293
Prathamesh Raut. <prathamesh1687@gmail.com>
2026-02-05 17:40:51 +0530

    Find Vowel's & Print Symbol

--- src/String_Program/Find_VowelsANDPrint__Symbol.java
+++ src/String_Program/Find_VowelsANDPrint__Symbol.java
@@ -2,6 +2,8 @@ package String_Program;
 
 public class Find_VowelsANDPrint__Symbol {
 	
+	
+	//This is Solve Vowels Using Hard Coded String
 	public void Print() {
 		String str="Stranger";
 		String res="";
@@ -16,9 +18,26 @@ public class Find_VowelsANDPrint__Symbol {
 		System.out.println("Using Symbol In Every Vowel's :"+res);
 	}
 	
+	
+	//This is Solve A Vowel's Issue Using Passing Parameter's In Function
+	public void Print1(String str) {
+		String res="";
+		for(int n =0;n<str.length();n++) {
+			char ch=str.charAt(n);
+			if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+				res=res+"@";
+			}else {
+				res=res+ch;
+			}
+		}
+		System.out.println("Using Symbol In Every Vowel's :"+res);
+	}
 public static void main(String[] args) {
 	Find_VowelsANDPrint__Symbol fvs= new Find_VowelsANDPrint__Symbol();
 	
 	fvs.Print();
+	
+	String str="Maharashtra";
+	fvs.Print1(str);
 }
 }
