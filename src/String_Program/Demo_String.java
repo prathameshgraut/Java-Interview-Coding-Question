a902a76dd7a16691dd983b50c4fa2892db506de9
Prathamesh Raut. <prathamesh1687@gmail.com>
2026-02-05 17:12:47 +0530

    Demo Of String

---
+++ src/String_Program/Demo_String.java
@@ -0,0 +1,11 @@
+package String_Program;
+
+public class Demo_String {
+
+	public static void main(String[] args) {
+		/*This Is A Simple Code Of String Pattern*/
+		String str="Hello I'm Simple String Code";    //Stored String Value
+		System.out.println(str);
+	}
+
+}
