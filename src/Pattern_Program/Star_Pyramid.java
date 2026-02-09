9bb6737788d5387120ecd09fde435d4102ea9204
Prathamesh Raut. <prathamesh1687@gmail.com>
2026-02-09 09:56:26 +0530

    Print Star Pyramid

---
+++ src/Pattern_Program/Star_Pyramid.java
@@ -0,0 +1,15 @@
+package Pattern_Program;
+
+public class Star_Pyramid {
+
+	public static void main(String[] args) {
+		/*Print Start Pyramid Pattern*/
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
