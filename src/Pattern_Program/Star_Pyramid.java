f4924e09691e1f5229b17f42ff1c6e76ecbfce0c
Prathamesh Raut. <prathamesh1687@gmail.com>
2026-02-09 09:57:04 +0530

    Star Pattern Pyramid

--- src/Pattern_Program/Star_Pyramid.java
+++ src/Pattern_Program/Star_Pyramid.java
@@ -3,7 +3,7 @@ package Pattern_Program;
 public class Star_Pyramid {
 
 	public static void main(String[] args) {
-		/*Print Start Pyramid Pattern*/
+		/*Print Start Pyramid Pattern */
 		for(int i=1;i<=5;i++) {
 			for(int j=1;j<=i;j++) {
 				System.out.print("*");
