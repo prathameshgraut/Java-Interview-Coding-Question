dbf05f5f361d9cb08a46820f4447e11d61b14dee
Prathamesh Raut. <prathamesh1687@gmail.com>
2026-02-09 10:02:32 +0530

    5 Number Table

---
+++ src/Pattern_Program/Create_5Table.java
@@ -0,0 +1,14 @@
+package Pattern_Program;
+
+public class Create_5Table {
+
+	public static void main(String[] args) {
+		/*Make a 5 Number Table*/
+		int num=5,res=0;
+		for(int i=1;i<=10;i++) {
+			res=num*i;
+			System.out.println(num+"*"+i+"="+res);
+		}
+	}
+
+}
